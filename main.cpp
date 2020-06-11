87dc4e4c0984f96171bb75eeab46037e0f41c538
Nirob Ahmed <neeerob.ahmed@gmail.com>
2020-06-12 01:58:30 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class system
+class System
 {
+    public:
+
     string message;
     int length;
     int password;
@@ -11,11 +13,25 @@ class system
     void encryption();
     void decreption();
 };
-
+void System :: encryption()
+{
+    cout<<"Enter password : ";
+    cin>>password;
+        if(password>10 || password <= 0)
+        {
+            cout<<"You entered more then 1 degit!"<<endl;
+            return encryption();
+        }
+        
+        cout<<"Your message will encrypt with your password!"<<endl;
+        cout<<"Enter a message to encrypt: "<<endl;
+        cin>>message;
+}
 
 
 int main()
 {
+    System runSystem;
     int option,password;
     cout<<"Enter your option: "<<endl;
     cout<<"1)Encrypt message 2)Decrept message "<<endl;
@@ -23,19 +39,12 @@ int main()
     switch (option)
     {
     case 1:
-
-        cout<<"You Want to encrypt messsage !"<<endl;
-        cout<<"You need to enter a 1 digit password! "<<endl; 
-        cout<<"You need to remember this password\nYou need this password to decrept this message! ;"<<endl;
-        cin>>password;
-        if(password>10 || password == 0)
-        {
-            
-        }
-        
-        cout<<"Your message will encrypt with your password!"<<endl;
-        
-        void encryption();
+        cout<<"-------------- Note :--------------------------"<<endl;
+        cout<<"* You Want to encrypt messsage !"<<endl;
+        cout<<"* You need to enter a 1 digit password! "<<endl; 
+        cout<<"* You need to remember this password!\n* You need this password to decrept this message! "<<endl;
+        cout<<"------------------------------------------------"<<endl;
+        runSystem.encryption();
         
         
 
